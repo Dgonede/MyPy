33e1b94d54f4df67e0bb3b78472bb38c6f2d7f90
Dgonede <dgonegonee@gmail.com>
2024-07-15 10:49:42 +0300

    1

--- PostgreSql/demo_1_create_table.py
+++ PostgreSql/demo_1_create_table.py
@@ -1,20 +1,20 @@
-from sqlalchemy import MetaData
-from sqlalchemy import Table
-from sqlalchemy import Column
-from sqlalchemy import Integer
-from sqlalchemy import String
-from db import engine
+# from sqlalchemy import MetaData
+# from sqlalchemy import Table
+# from sqlalchemy import Column
+# from sqlalchemy import Integer
+# from sqlalchemy import String
+# from db import engine
 
 
-metadata = MetaData()
+# metadata = MetaData()
 
-authors_table = Table(
-    name="authors",
-    metadata,
-    Column(name="id", Integer, primary_key=True),
-    Column(name="username", String, nullable=False, unique=True),
-    Column(name="email", String, nullable=True, unique=True),
-)
+# authors_table = Table(
+#     name="authors",
+#     metadata,
+#     Column(name="id", Integer, primary_key=True),
+#     Column(name="username", String, nullable=False, unique=True),
+#     Column(name="email", String, nullable=True, unique=True),
+# )
 
-def create_table():
-    metadata.create_all(bind=engine)
+# def create_table():
+#     metadata.create_all(bind=engine)
