b11215c01c5f1d68373eeeb4d553fc43950c4653
Dgonede <dgonegonee@gmail.com>
2024-07-24 10:26:22 +0300

    1

--- homework_04/main.py
+++ homework_04/main.py
@@ -68,14 +68,14 @@ async def fetch_all_posts_with_authors(
 
 
 async def async_main():
-    await create_tables()
+    # await create_tables()
     async with Session() as session:
         await create_user(session, username="lone", email="lone@admin.com")
-        gane: User = await create_user(session, username="gane", email=None)
+        Dan: User = await create_user(session, username="gane", email=None)
         post_pg: Post = await create_post(
             session=session,
             title="Reander post",
-            user_id=gane.id,
+            user_id=Dan.id,
            
         )
         print("post pg:", post_pg)
