05fa2d86ff2977962a8626ef7fd163c0519530cb
Dgonede <dgonegonee@gmail.com>
2024-07-31 09:56:00 +0300

    1

--- homework_05/app.py
+++ homework_05/app.py
@@ -5,16 +5,18 @@ from views.products import products_app
 app = Flask(__name__)
 app.register_blueprint(products_app)
 
-@app.route("/")
+@app.route("/", endpoint="index")
 def hello():
-    return "<h1>Hello World</h1>"
+    return render_template(
+        "index.html"
+    )
 
 
 @app.route("/hello/<name>/")
 def hello_name(name):
     name=name.strip()
     return render_template(
-        "index.html",
+        "hello.html",
         name=name,
     )
 
