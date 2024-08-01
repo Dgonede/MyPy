8b7a2afaedebc7c43b96fef30d4f6a8af9cfa75f
Dgonede <dgonegonee@gmail.com>
2024-08-01 15:22:36 +0300

    1

--- homework_04/main.py
+++ homework_04/main.py
@@ -1,8 +1,6 @@
 import asyncio
 from collections.abc import Sequence
 from sqlalchemy import desc, select
-import logging
-import aiohttp
 from sqlalchemy.orm import selectinload
 from sqlalchemy.ext.asyncio import AsyncSession
 from .models import (
@@ -13,81 +11,88 @@ from .models import (
     Post, 
     )
 
-# Настройка логирования
-log = logging.getLogger(__name__)
-
-
-# Определение URL для получения данных
-USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
-POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
-
-
-
-# Асинхронные функции для работы с базой данных
-async def create_user(session: AsyncSession, username: str, email: str) -> User:
+async def create_tables():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    
+async def create_user(
+    session: AsyncSession,
+    username: str,
+    email: str | None = None,
+) -> User:
     user = User(username=username, email=email)
     session.add(user)
     await session.commit()
-    await session.refresh(user)
     return user
 
-async def create_post(session: AsyncSession, title: str, body: str, user_id: int) -> Post:
-    post = Post(title=title, body=body, user_id=user_id)
+
+async def create_post(
+    session: AsyncSession,
+    title: str,
+    user_id: int,
+    body: str,
+) -> Post:
+    post = Post(title=title, user_id=user_id, body=body)
     session.add(post)
     await session.commit()
-    await session.refresh(post)
     return post
 
-async def fetch_all_users(session: AsyncSession):
-    result = await session.execute(select(User))
-    return result.scalars().all()
 
-async def fetch_all_posts(session: AsyncSession):
-    result = await session.execute(select(Post))
-    return result.scalars().all()
+async def fetch_all_users(session: AsyncSession) -> Sequence[User]:
+    stmt = select(User).order_by(desc(User.username))
+    result = await session.scalars(stmt)
+    users = result.all()
+    return users
 
-# Функция для получения данных из API
-async def fetch_json(url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.json()
-            return data
 
-async def fetch_users_data():
-    return await fetch_json(USERS_DATA_URL)
+async def fetch_all_posts(session: AsyncSession) -> Sequence[Post]:
+    stmt = select(Post).order_by(Post.id)
+    result = await session.scalars(stmt)
+    posts = result.all()
+    return posts
 
-async def fetch_posts_data():
-    return await fetch_json(POSTS_DATA_URL)
 
-async def main():
-    logging.basicConfig(level=logging.INFO)
-    log.warning("Starting main")
-    
+
+async def fetch_all_posts_with_authors(
+    session: AsyncSession,
+) -> Sequence[Post]:
+    stmt = (
+        select(Post)
+        .options(
+            selectinload(Post.user),
+            selectinload(Post.body)
+        )
+        .order_by(Post.id)
+    )
+    result = await session.execute(stmt)
+    return result
+
+   
+async def async_main():
+    await create_tables()
     async with Session() as session:
-        # Получение данных пользователей и постов
-        users_data = await fetch_users_data()
-        posts_data = await fetch_posts_data()
+        await asyncio.gather(create_user(session, username="admin", email="admin@admin.com"))
+        bob: User = await create_user(session, username="bob", email=None)
+        john: User = await create_user(session, username="john", email=None)
+        greg: User = await create_user(session, username="greg", email=None)
+        post_pg: Post = await asyncio.gather(create_post(
+            session=session,
+            title="PostgreSQL news",
+            user_id=greg.id,
+            body="PUSTO",
+        ))
+
         
-        # Создание пользователей в базе данных
-        user_map = {}  # Словарь для сопоставления username и id
-        for user_data in users_data:
-            user = await create_user(session, username=user_data['username'], email=user_data['email'])
-            user_map[user_data['id']] = user.id  # Сохраняем соответствие userId и id в базе данных
-        
-        # Создание постов в базе данных
-        for post_data in posts_data:
-            user_id = user_map.get(post_data['userId'])  # Получаем id пользователя из user_map
-            if user_id:  # Проверяем, существует ли пользователь
-                await create_post(session, title=post_data['title'], body=post_data['body'], user_id=user_id)
-        
-        # Получение всех пользователей и постов
-        users = await fetch_all_users(session)
-        posts = await fetch_all_posts(session)
-        
-        log.info("Fetched users: %s", users)
-        log.info("Fetched posts: %s", posts)
+    await asyncio.gather(fetch_all_users(session))  
+    await asyncio.gather(fetch_all_posts(session))  
     
-    log.warning("Finished main")
+        
+        
+        
+def main():
+    async_main()        
+      
+       
 
 if __name__ == "__main__":
     asyncio.run(main())
